dbdd2c8bf20f1efa17b2b591dab59e47ccbacdd3
Kavinshree82 <kavinshree82@gmail.com>
2026-06-06 13:55:40 +0530

    Solve: Palindrome Number

---
+++ palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x)
+     {
+       
+       int num=x;
+       int rev=0;
+
+       while(num>0)
+       {
+        int r=num%10;
+        rev=rev*10+r;
+        num=num/10;
+       }
+
+       return x==rev;
+        
+     }
+}
