70c9fa3f89a8154038ddc3fccc83e32add15566d
Kavinshree82 <kavinshree82@gmail.com>
2026-06-13 18:06:00 +0530

    Solve: Majority Element

---
+++ majority-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int cand=0;
+        int count=0;
+
+        for(int x:nums)
+        {
+            if(count==0)
+            {
+                cand=x;
+            }
+            if(x==cand)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+    
+    return cand;
+    }
+}
