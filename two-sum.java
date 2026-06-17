459c08c78652ed1f3655264218d47795221322e0
Kavinshree82 <kavinshree82@gmail.com>
2026-06-17 22:35:18 +0530

    Solve: Two Sum

---
+++ two-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) 
+    {
+      HashMap<Integer,Integer> map=new HashMap<>();
+
+      for(int i=0;i<nums.length;i++)
+      {
+        int need=target-nums[i];
+
+        if(map.containsKey(need))
+        {
+         int j=map.get(need);
+         return new int[]{j,i};   
+        }
+        map.put(nums[i],i);
+      }
+
+      return new int[]{};
+    }
+}
