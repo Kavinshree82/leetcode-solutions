ea5c3a663dedd5c48042529a6465fbf0eea4e3dd
Kavinshree82 <kavinshree82@gmail.com>
2026-06-25 10:59:55 +0530

    Solve: Missing Number

---
+++ missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int n=nums.length;
+        int sum=0;
+        int miss=0;
+        int total=0;
+
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        total = n*(n+1)/2;
+
+        miss=total-sum;
+        return miss;
+    }
+}
