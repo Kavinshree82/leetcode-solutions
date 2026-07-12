6b8bb398107a1ac56f2417f3511b1a8a735c927d
Kavinshree82 <kavinshree82@gmail.com>
2026-07-12 21:23:08 +0530

    Solve: Binary Gap

---
+++ binary-gap.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int binaryGap(int n) {
+        int prev = -1;
+        int maxGap = 0;
+        int pos = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (prev != -1) {
+                    maxGap = Math.max(maxGap, pos - prev);
+                }
+                prev = pos;
+            }
+
+            n >>= 1;
+            pos++;
+        }
+
+        return maxGap;
+    }
+}
